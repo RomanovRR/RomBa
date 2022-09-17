7ef995539da8b5262e518d4fbe73f5178ed6dd0a
Roman Romanov <rom_roma@mail.ru>
2022-09-17 17:53:34 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/alarmclock/MainActivity.java
+++ app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault() );
 
         setAlarmBtn = findViewById(R.id.SetupClockButton);
